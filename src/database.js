e78591f84c41d512df8154719b61e2ebb0fba395
Anna <a.karlysheva@ukma.edu.ua>
2019-04-20 12:07:02 +0300

    function addPic

--- src/database.js
+++ src/database.js
@@ -1,13 +1,13 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "omnibank"
-});
+var con;
 
 function connect() {
+    con  = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "omnibank"});
     try {
         con.connect(function (err) {
             if (err) throw err;
@@ -44,7 +44,7 @@ function addPassport(login, nameQ, middleNameQ, surnameQ, birthQ, sexQ, countryQ
     var idQ = getUserID(login);
     var sql = "INSERT INTO passport (name, middle_name, surname, birth,  sex, country, expiry, number, userID) VALUES ("
         + "\'" + nameQ + "\', \'" + middleNameQ + "\', \'" + surnameQ + "\', \'"
-        + birthQ + "\', " + sexQ + ", \'" + countryQ + "\',\'"
+        + birthQ + "\', \'" + sexQ + "\', \'" + countryQ + "\',\'"
         + dateExpiryQ + "\'," + numberQ + "," + idQ + ")";
     con.query(sql, function (err, result) {
         if (err) throw err;
@@ -52,3 +52,23 @@ function addPassport(login, nameQ, middleNameQ, surnameQ, birthQ, sexQ, countryQ
     });
 
 }
+
+function getUserLogin(loginQ, passwordQ) {
+    con.query("SELECT * FROM user WHERE login = " + "\'" + loginQ + "\'", function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        if (result.length==0)
+            return "no_login";
+        console.log(result[0].login, result[0].password);
+        if (result[0].password!=passwordQ)
+            return "false"
+        return "true";
+
+
+    });
+}
+
+function addPic(loginQ, pathQ, roleQ, name){
+
+}
+console.log(getUserLogin('heyy', 'me'));
