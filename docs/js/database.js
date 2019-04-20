e830026cdb4bf4d9dd9b75007ba8f655caa3723a
Anna <a.karlysheva@ukma.edu.ua>
2019-04-20 11:15:10 +0300

    coonection function

--- docs/js/database.js
+++ docs/js/database.js
@@ -7,10 +7,17 @@ var con = mysql.createConnection({
     database: "omnibank"
 });
 
-con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+function connect() {
+    try {
+        con.connect(function (err) {
+            if (err) throw err;
+            console.log("Connected!");
+        });
+    }
+    catch (e) {
+
+    }
+}
 
 function addUser(loginQ, eamilQ, passwordQ, nameQ, middleNameQ, surnameQ, birthQ, sexQ, countryQ) {
     var sql = "INSERT INTO user (login, email, password, name, middle_name, surname, birth,sex,country) VALUES ("
@@ -24,25 +31,24 @@ function addUser(loginQ, eamilQ, passwordQ, nameQ, middleNameQ, surnameQ, birthQ
 //throws ER_DUP_ENTRY error if this login is already in the table
 }
 
-function getUserID(login){
-    con.query("SELECT id FROM user WHERE login = "+"\'"+login+"\'", function (err, result) {
+function getUserID(login) {
+    con.query("SELECT id FROM user WHERE login = " + "\'" + login + "\'", function (err, result) {
         if (err) throw err;
         console.log(result);
         return result;
     });
 }
+
 //todo
 function addPassport(login, nameQ, middleNameQ, surnameQ, birthQ, sexQ, countryQ, dateExpiryQ, numberQ) {
     var idQ = getUserID(login);
     var sql = "INSERT INTO passport (name, middle_name, surname, birth,  sex, country, expiry, number, userID) VALUES ("
         + "\'" + nameQ + "\', \'" + middleNameQ + "\', \'" + surnameQ + "\', \'"
         + birthQ + "\', " + sexQ + ", \'" + countryQ + "\',\'"
-        + dateExpiryQ + "\'," + numberQ+ "," +idQ + ")";
+        + dateExpiryQ + "\'," + numberQ + "," + idQ + ")";
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("1 record inserted");
     });
 
 }
-
-addUser("hello", "hello", "anya", "kesa", "1999-11-12", "kiev", 1);
