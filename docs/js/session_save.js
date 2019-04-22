6bfab368deee368088cc1b0d4d7518fa2a223b45
Соломія Андрусів <solomiia.andrusiv@ukma.edu.ua>
2019-04-22 19:27:25 +0300

    temp design commit

--- docs/js/session_save.js
+++ docs/js/session_save.js
@@ -4,3 +4,19 @@ $(document).on('click', '.done-button', function () {
     let login = document.getElementById("login").value;
     sessionStorage.setItem('login',login);
 });
+
+$("#identity").click(function(){
+    window.location.href='identity.html'
+});
+$("#bank_cards").click(function(){
+    window.location.href='bank_cards.html'
+});
+$("#certificate").click(function(){
+    window.location.href='certificate.html'
+});
+$("#other").click(function(){
+    window.location.href='other.html'
+});
+$("#tickets").click(function(){
+    window.location.href='tickets.html'
+});
